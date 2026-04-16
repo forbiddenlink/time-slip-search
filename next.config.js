60f643f5f72b178ce0819ec5147092572f7db6ad
Elizabeth Stein <purplegumdropz@gmail.com>
2026-04-16 07:33:45 -0400

    chore: add next-sitemap, bundle-analyzer, deps

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+import withBundleAnalyzerInit from "@next/bundle-analyzer";
+const withBundleAnalyzer = withBundleAnalyzerInit({ enabled: process.env.ANALYZE === "true" });
+
 /* eslint-env node */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
